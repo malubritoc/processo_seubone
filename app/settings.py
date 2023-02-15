8aec6b9f44536716de557cf3ec738a056c56ba2d
Maria Luìsa <malubritoc@MacBook-Air-de-Maria.local>
2023-02-15 19:14:04 -0300

    iniciando a estilizacao

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'core'
 ]
 
+STATICFILES_DIRS = [    
+    BASE_DIR / "static",
+    ]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -116,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
